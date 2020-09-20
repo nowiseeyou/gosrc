745f1b1a3671f3b1173e8ad8c9511d2e43af41e9
nowiseeyou <1182801311@qq.com>
2020-09-20 17:07:29 +0800

    fx

--- hello.go
+++ hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	
+	"github.com/nowiseeyou/stringutil"
+)
 
 func main()  {
-	fmt.Printf("Hello , source,xixi Mansaode\n")
+	s := "Hello , source,xi xi Msd"
+	reverse := stringutil.Reverse(s)
+	fmt.Printf(reverse)
 }
